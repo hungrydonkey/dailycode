73c54095b325bc8967e65d4e601b2eaf4b5ede77
hungrydonkey <35836426+hungrydonkey@users.noreply.github.com>
2019-04-15 22:48:38 +0800

    Update nqueen.c

--- nqueen.c
+++ nqueen.c
@@ -1,23 +1,23 @@
-void dis(int n,int a[][n]){
+static inline void dis(int n,int *a){
     for(int i = 0;i < n;++i){
         for(int j = 0;j < n;++j)
-            a[i][j] == 1 ? printf("+ ") : printf("* ");
+            *(a+i*n+j) == 1 ? printf("+ ") : printf("* ");
         puts("");
     }
 }
 
-int panduan(int n,int a[][n],int row,int clo){
+static int panduan(int n,int *a,int row,int clo){
     for(int i = clo,k = clo;row >= 0 ;--row,--i,++k){
-        if(a[row][clo] == 1) return 0;
+        if(*(a+row*n+clo) == 1) return 0;
         if(i >= 0)
-            if(a[row][i] == 1) return 0;
+            if(*(a+row*n+i) == 1) return 0;
         if(k < n)
-         if(a[row][k] == 1)  return 0;
+         if(*(a+row*n+k) == 1)  return 0;
     }
     return 1;
 }
 
-void findindex(int n,int a[][n],int row){
+static void findindex(int n,int *a,int row){
     if(row == n){
         dis(n,a);
         puts("");
@@ -25,9 +25,9 @@ void findindex(int n,int a[][n],int row){
     }
     for(int i = 0;i < n;++i)
         if(panduan(n,a,row,i)){
-            a[row][i] = 1;
+            *(a+row*n+i) = 1;
             findindex(n,a,row+1);
-            a[row][i] = 0;
+            *(a+row*n+i) = 0;
         }
 }
 
