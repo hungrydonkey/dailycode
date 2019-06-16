416364bba617ae36bab02db973bcd76062ff4de2
hungrydonkey <35836426+hungrydonkey@users.noreply.github.com>
2019-06-16 22:42:19 +0800

    Update BTC.py

--- BTC.py
+++ BTC.py
@@ -3,10 +3,10 @@ import json
 
 file = open('bitzb.txt','w')
 bitchain = 'https://chain.api.btc.com/v3'
-resp = requests.get(bitchain+'/block/2')
+
 
 for i in range(10):
-    resp = requests.get(bitchain+'/block'+str(i))
+    resp = requests.get(bitchain+'/block/'+str(i))
     #html = json.loads(resp.text)
     file.write(resp.text)
 
